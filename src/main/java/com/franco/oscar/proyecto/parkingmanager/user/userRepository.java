f22e0ddcadb82d3af02212155ba483c84249ec47
Franco <fcas658@g.educaand.es>
2023-01-25 21:06:14 +0100

    manager parking

--- src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
+++ src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
@@ -2,8 +2,6 @@ package com.franco.oscar.proyecto.parkingmanager.user;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.list;
-
 import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 public class userRepository {
