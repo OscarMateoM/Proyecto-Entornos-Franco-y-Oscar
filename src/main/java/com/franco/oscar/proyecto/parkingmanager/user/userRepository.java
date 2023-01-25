f37b6cd8a2e69da9e26083e6856a08d2b4a4b5fb
Oscar <omatmar964@g.educaand.es>
2023-01-25 20:57:01 +0100

    manager parking

--- src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
+++ src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
@@ -2,7 +2,9 @@ package com.franco.oscar.proyecto.parkingmanager.user;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.h2.engine.User;
+import java.util.list;
+
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 public class userRepository {
     private ArrayList<User> usuarios;
@@ -10,6 +12,8 @@ public class userRepository {
         usuarios = new ArrayList<>();
     }
     public List<User> getAll() {
+        this.usuarios.clear();
+        this.usuarios.add(new User("email: aomatmar964@g.educaand.es","name: Oscar Mateo Martin"));
         return this.usuarios;
     }
 }
