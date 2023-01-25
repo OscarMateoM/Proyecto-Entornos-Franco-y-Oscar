80b13e5200ce93b659c0f866029fd3aef84632ae
Franco <fcas658@g.educaand.es>
2023-01-25 20:24:53 +0100

    manager parking

--- src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userController.java
+++ src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userController.java
@@ -1,5 +1,16 @@
 package com.franco.oscar.proyecto.parkingmanager.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class userController {
     
+    private userRepository repository = new userRepository();
+
+    @GetMapping("/usuarios")
+    public String displayUsers(Model model) {
+        model.addAllAttribute(attributeName: "usuarios", repository.getAll());
+        return "listausuarios";
+    }
 }
