97628814c277e085388d8e2f043bdda662a39a1d
fcas658 <fcas658@g.educaand.es>
2023-02-01 20:37:43 +0100

    test

--- src/test/java/com/franco/oscar/proyecto/parkingmanager/testUser.java
+++ src/test/java/com/franco/oscar/proyecto/parkingmanager/testUser.java
@@ -1,5 +1,26 @@
 package com.franco.oscar.proyecto.parkingmanager;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.franco.oscar.proyecto.parkingmanager.user.User;
+
 public class testUser {
+
+    @Test
+    public void check_user_name() {
+        // Arrange
+        User david = new User("Oscar", "Mateo",  "Martin", null);
+        String expected = "Oscar";
+
+        // Act
+        String actual = david.getFirstName();
+
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
     
 }
