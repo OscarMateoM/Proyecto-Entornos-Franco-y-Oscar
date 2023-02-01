49da7c1a16a4efc7fbfc6077714bbcc0aa5b75f7
fcas658 <fcas658@g.educaand.es>
2023-02-01 20:08:16 +0100

    manager parking

--- src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
+++ src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
@@ -10,7 +10,7 @@ public class userRepository {
     }
     public List<User> getAll() {
         this.usuarios.clear();
-        this.usuarios.add(new User("email: aomatmar964@g.educaand.es","name: Oscar Mateo Martin"));
+        this.usuarios.add(new User("Oscar","Mateo", "Martin", null));
         return this.usuarios;
     }
 }
