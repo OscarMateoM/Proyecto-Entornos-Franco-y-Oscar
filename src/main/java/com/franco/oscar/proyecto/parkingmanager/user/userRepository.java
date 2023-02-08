20a377156890c42a22385869fca92d581c3de77c
Oscar <omatmar964@g.educaand.es>
2023-02-08 20:58:48 +0100

    new commit

--- src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
+++ src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
-@Repository
+@Repository 
 public class userRepository {
     private ArrayList<User> usuarios;
     public userRepository() {
@@ -13,6 +13,7 @@ public class userRepository {
     public List<User> getAll() {
         this.usuarios.clear();
         this.usuarios.add(new User("Oscar","Mateo", "Martin", null));
+        this.usuarios.add(new User("Franco Tomas","Casco", null, null));
         return this.usuarios;
     }
 }
