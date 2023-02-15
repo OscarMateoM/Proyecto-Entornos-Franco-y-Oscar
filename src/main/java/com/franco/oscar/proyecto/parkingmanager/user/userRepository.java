eed0c8190dab0dff870c880b6aad9a5bdf6d5298
fcas658 <fcas658@g.educaand.es>
2023-02-15 19:45:59 +0100

    cambios pom

--- src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
+++ src/main/java/com/franco/oscar/proyecto/parkingmanager/user/userRepository.java
@@ -2,6 +2,7 @@ package com.franco.oscar.proyecto.parkingmanager.user;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import org.springframework.stereotype.Repository;
 @Repository 
